0773278c8716c6c54f997f29d0db7cc073698f27
Finalelele <Akkountforgames@yandex.ru>
2026-04-08 20:47:30 +0500

    Update spy.lua

--- spy.lua
+++ spy.lua
@@ -21,7 +21,7 @@ Main.Draggable = true
 
 local Title = Instance.new("TextLabel", Main)
 Title.Size = UDim2.new(1, 0, 0, 35)
-Title.Text = "QUANTUM SPY v2.7"
+Title.Text = "QUANTUM SPY v2.8"
 Title.TextColor3 = Color3.new(1, 1, 1)
 Title.BackgroundColor3 = Color3.fromRGB(45, 45, 45)
 
